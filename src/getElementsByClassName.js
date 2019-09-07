aa750443dd68d8964a767d0d13f4a930f89fc619
Charley Chang <charleyarby@gmail.com>
2019-09-07 09:24:36 -0700

    finished the sprint

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,53 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
+​
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var result = [];
+  var searchNode = function(nodes) {
+    // if ( nodes.childNodes.length === 0 ) {
+​
+    if (nodes.classList !== undefined && nodes.classList.contains(className) ) {
+​
+      result.push(nodes);
+    }
+​
+    // }
+    if (nodes.childNodes.length > 0 ) {
+      for ( var i = 0; i < nodes.childNodes.length; i++) {
+​
+        searchNode(nodes.childNodes[i]);
+      }
+    }
+​
+​
+​
+  };
+​
+​
+  searchNode(document.body);
+  console.log(result);
+  return result;
 };
+​
+// var getElementsByClassName = function(className) {
+//   // your code here
+//   var result = [];
+//   var searchNode = function(nodes) {
+//     if ( nodes.children !== true ) {
+//       if ( nodes.classList.contains(className) ) {
+//         result.push(nodes);
+//       }
+//     }
+//     if (nodes.children) {
+//       for ( var i = 0; i < nodes.children.length; i++) {
+//         searchNode(nodes.children[i]);
+//       }
+//     }
+//     return result;
+//   };
+//   console.log(result);
+//   return searchNode(document.body);
+// };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,70 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
-
+​
 // but you don't so you're going to write it from scratch:
-
+​
 var stringifyJSON = function(obj) {
   // your code goes here
+  if ( typeof obj === 'number') {
+    return '' + obj + '';
+  }
+  if ( obj === null) {
+    return 'null';
+  }
+  if ( obj === true) {
+    return 'true';
+  }
+  if ( obj === false) {
+    return 'false';
+  }
+​
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+​
+  if ( Array.isArray(obj) === true) {
+    var result = '';
+    if (obj.length === 0) {
+      return '[' + ']';
+    } else if (obj.length > 0) {
+      result += '[';
+      for (var i = 0; i < obj.length; i++) {
+        result += stringifyJSON(obj[i]);
+        if (i !== obj.length - 1) {
+          result += ',';
+        }
+      }
+      result += ']';
+    }
+    return result;
+  }
+  if ( typeof obj === 'object') {
+    var result = '';
+    if (Object.keys(obj).length === 0) {
+      return '{' + '}';
+    } else {
+      result += '{';
+      var count = 0;
+      var numKeys = Object.keys(obj).length;
+      for ( var key in obj ) {
+        if ( typeof obj[key] === 'function' || key === 'undefined') {
+          count++;
+          if ( count === numKeys) {
+            result += '}';
+            break;
+          } else {
+            continue;
+          }
+        }
+        result += '"' + key + '"' + ':' + stringifyJSON(obj[key]);
+        count++;
+        if ( count < numKeys) {
+          result += ',';
+        } else if ( count === numKeys) {
+          result += '}';
+        }
+      }
+      return result;
+    }
+  }
 };
